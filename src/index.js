71996006aa6e1e137a8c42e26ea3997a9230a2a4
GuillemXanxo <jscodebaker@gmail.com>
2022-03-07 18:37:55 +0100

    Added ddbb connection in server index

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const debug = require("debug")("pallars:root");
 const chalk = require("chalk");
 const serverUp = require("./server/serverUp");
+const connectDB = require("./db/index");
 const { app } = require("./server/index");
 
+const connectionString = process.env.MONGODB_STRING;
 const port = process.env.PORT || 3000;
 
 (async () => {
   try {
+    await connectDB(connectionString);
     await serverUp(port, app);
     debug(chalk.greenBright(`Server-up in ${port}`));
   } catch (error) {
