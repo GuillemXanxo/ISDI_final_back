6ae5b48943c825cfc39b0182fae93a77161325d2
GuillemXanxo <jscodebaker@gmail.com>
2022-03-08 11:42:22 +0100

    viatgesRouters in server index and errors middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const viatgesRouters = require("./routers/viatgesRouters");
+const { notFoundError, generalError } = require("./middlewares/errors");
 
 const app = express();
 
@@ -10,5 +12,10 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/viatges", viatgesRouters);
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = { app };
