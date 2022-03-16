65002a6f9407f463a4006c78b3c8c6666d139674
GuillemXanxo <jscodebaker@gmail.com>
2022-03-16 09:39:54 +0100

    Added userLogin controller

--- src/server/controllers/usuariControllers.js
+++ src/server/controllers/usuariControllers.js
@@ -1,3 +1,30 @@
-const userLogin = () => {};
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Usuari = require("../../db/models/Usuari");
+
+const userLogin = async (req, res, next) => {
+  const { usuari, contrassenya } = req.body;
+  const user = await Usuari.findOne({ usuari });
+  if (!user) {
+    const error = new Error("Alguna de les teves dades no és vàlida");
+    error.status = 401;
+    return next(error);
+  }
+  const isRightPassword = await bcrypt.compare(
+    contrassenya,
+    usuari.contrassenya
+  );
+  if (!isRightPassword) {
+    const errorWrongPwd = new Error("Alguna de les teves dades no és vàlida");
+    errorWrongPwd.status = 401;
+    return next(errorWrongPwd);
+  }
+  const userData = {
+    nom: usuari.nom,
+    id: usuari.id,
+  };
+  const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return res.json({ token });
+};
 
 module.exports = userLogin;
