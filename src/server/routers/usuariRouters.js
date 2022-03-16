b03274f2b9e441a73d3e922a25237de86672de83
GuillemXanxo <jscodebaker@gmail.com>
2022-03-16 09:24:02 +0100

    Created encrypt for bcrypt encryption

--- src/server/routers/usuariRouters.js
+++ src/server/routers/usuariRouters.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const userLogin = require("../controllers/usuariControllers");
 
 const router = express.Router();
+
+router.post("/login", userLogin);
 
 module.exports = router;
