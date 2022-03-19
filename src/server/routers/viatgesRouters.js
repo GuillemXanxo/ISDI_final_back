395250b08ab52ce54a837aaca4295f6523e4a637
GuillemXanxo <jscodebaker@gmail.com>
2022-03-19 17:18:27 +0100

    Added router, controller and test router for detail

--- src/server/routers/viatgesRouters.js
+++ src/server/routers/viatgesRouters.js
@@ -4,12 +4,14 @@ const {
   getViatgesCrono,
   deleteViatge,
   createViatge,
+  getThisViatge,
 } = require("../controllers/viatgesControllers");
 
 const router = express.Router();
 
 router.get("/crono", getViatgesCrono);
 router.delete("/:id", deleteViatge);
+router.get("/:id", getThisViatge);
 router.post("/crear", createViatge);
 
 module.exports = router;
